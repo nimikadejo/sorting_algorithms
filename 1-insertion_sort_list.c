405987acc0b471909468bddab07d89ec4f27d30c
nimikadejo <kadejotoluwanimi@gmail.com>
2022-10-12 13:55:53 -0700

    insert

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,50 +1,35 @@
 #include "sort.h"
 #include <stdio.h>
+
 /**
  * insertion_sort_list - insertion sort algorithm for doubly linked list
  * @list: pointer to array to sort
+ * Return: Nothing
 */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *node, *tmp, *next, *tmp_next, *tmp_prev;
-	int i = 0;
+	listint_t *swap_node, *next_swap;
 
-	if (list == NULL || (*list)->next == NULL)
+	if (list == NULL || *list == NULL)
 		return;
-	node = *list;
-	node = node->next;
-	while (node != NULL)
+	swap_node = (*list)->next;
+	while (swap_node != NULL)
 	{
-		tmp = node;
-		next = node->next;
-		tmp_next = tmp->next;
-		tmp_prev = tmp->prev;
-
-		while (tmp->prev != NULL && tmp_prev->n > tmp->n)
+		next_swap = swap_node->next;
+		while (swap_node->prev != NULL && swap_node->prev->n > swap_node->n)
 		{
-			if (tmp_next != NULL)
-			{
-				tmp_next->prev = tmp_prev;
-				tmp_prev->next = tmp_next;
-			}
-			if (tmp_prev->prev == NULL)
-			{
-				tmp_prev->prev = tmp;
-				*list = tmp;
-				tmp->prev = NULL;
-			}
+			swap_node->prev->next = swap_node->next;
+			if (swap_node->next != NULL)
+				swap_node->next->prev = swap_node->prev;
+			swap_node->next = swap_node->prev;
+			swap_node->prev = swap_node->next->prev;
+			swap_node->next->prev = swap_node;
+			if (swap_node->prev == NULL)
+				*list = swap_node;
 			else
-			{
-				tmp->prev = tmp_prev->prev;
-				tmp_prev->prev->next = tmp;
-				tmp_prev->prev = tmp;
-			}
-			tmp->next = tmp_prev;
-			tmp_prev = tmp->prev;
-			tmp_next = tmp->next;
+				swap_node->prev->next = swap_node;
 			print_list(*list);
 		}
-		node = next;
-		i++;
+		swap_node = next_swap;
 	}
 }
