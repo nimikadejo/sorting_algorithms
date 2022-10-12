917196973b5df1131d50acb9666ef2584ebeb6ed
nimikadejo <kadejotoluwanimi@gmail.com>
2022-10-12 12:51:56 -0700

    Betty error

--- sort.h
+++ sort.h
@@ -15,9 +15,9 @@ void print_list(const listint_t *list);
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void bubble_sort(int *array, size_t size);
